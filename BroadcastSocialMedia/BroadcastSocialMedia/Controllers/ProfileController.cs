95f5e048e876509273801257d8f97102342bdf46
Elka <169077740+YasinElka@users.noreply.github.com>
2025-12-10 20:45:56 +0100

    Added Name to user profile

---
+++ BroadcastSocialMedia/BroadcastSocialMedia/Controllers/ProfileController.cs
@@ -0,0 +1,36 @@
+﻿using BroadcastSocialMedia.Models;
+using BroadcastSocialMedia.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BroadcastSocialMedia.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager; // UserManager for ApplicationUser
+
+        public ProfileController(UserManager<ApplicationUser>userManager)
+        {
+           _userManager = userManager; // Dependency injection of UserManager
+        }
+        public async Task <IActionResult> Index()
+        {
+            var user = await _userManager.GetUserAsync(User); // Get the current user's ID
+           
+            var ViewModel = new ViewModels.ProfileIndexViewModel
+            {
+                Name = user.Name ?? "" // Set the Name property in the ViewModel
+            };
+            return View(ViewModel);
+        }
+
+        public async Task<IActionResult> Update(ProfileIndexViewModel viewModel)
+        {
+            var user = await _userManager.GetUserAsync(User);
+            user.Name = viewModel.Name; // Update the Name property
+            await _userManager.UpdateAsync(user); // Save changes to the user
+
+            return RedirectToAction("/"); // Redirect to the Index action
+        }
+    }
+}
---
+++ BroadcastSocialMedia/BroadcastSocialMedia/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BroadcastSocialMedia.Models
+{
+    public class ApplicationUser : IdentityUser
+
+    {
+        public string? Name { get; set; } // defines a Name property for the user
+
+
+    }
+}
